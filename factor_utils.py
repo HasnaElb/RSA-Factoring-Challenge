cebca116d01f6db5d8c2b18c43f16eab93dfab17
Hasna <hasna.elboukhari.neuro@gmail.com>
2023-12-16 03:17:48 -0800

    file created

--- factor_utils.py
+++
@@ -1,8 +0,0 @@
-#factor_utils.py
-
-def factorize(n):
-    #Function to find factorization of a number
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return i, n // i
-        return None, None
